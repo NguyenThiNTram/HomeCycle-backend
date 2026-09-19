6355c6849997dac8a168385f8ac425c153662009
NguyenThiNTram <ngoctram31204@gmail.com>
2026-09-19 22:26:24 +0700

    add(Product-type): add api get space usages

---
+++ HomeCycle.Application/DTOs/Responses/Products/SpaceUsageResponse.cs
@@ -0,0 +1,8 @@
+namespace HomeCycle.Application.DTOs.Responses.Products
+{
+    public class SpaceUsageResponse
+    {
+        public int Value { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
